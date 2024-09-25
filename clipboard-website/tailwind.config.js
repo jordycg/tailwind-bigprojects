6dc94406b89f4e0db6c4265c34875a89f0961fca
jordycg <jordychonggayle@gmail.com>
2024-09-24 23:16:39 -0400

    Had some trouble with the Tailwind directories and folders but I knew what the tutorial was doing for the most part

--- clipboard-website/tailwind.config.js
+++ clipboard-website/tailwind.config.js
@@ -1,23 +1,24 @@
-module.exports = { 
+module.exports = {
     content: ["./src/**/*.{html,js}", "./public/**/*.html", "./*.html"],
     theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      extend: {
+        colors: {
+          strongCyan: 'hsl(171, 66%, 44%)',
+          lightBlue: 'hsl(233, 100%, 69%)',
+          darkGrayishBlue: 'hsl(210, 10%, 33%)',
+          grayishBlue: 'hsl(201, 11%, 66%)',
         },
-        extend: {
-            colors: {
-                strongCyan: 'hsl(171, 66%, 44%)',
-                lightBlue: 'hsl(233, 100%, 69%)',
-                darkGrayishBlue: 'hsl(210, 10%, 33%)',
-                grayishBlue: 'hsl(201, 11%, 66%)',
-            },
-            fontFamily: {
-                sans: ['Bai Jamjuree', 'sans-serif'],
-            }
+        fontFamily: {
+          sans: ['Bai Jamjuree', 'sans-serif'],
         },
+      },
     },
     plugins: [],
-}
+  }
+  
